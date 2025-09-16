d0b454124a36ae0712204d84d046ad6fd8c7ce0d
nicvlt <nicolas.violot.pro@gmail.com>
2025-09-16 14:45:35 +0200

    refactor: migrate App component to Tailwind CSS
    Remove App.css and replace with Tailwind utility classes.
    Convert app container layout to Tailwind flexbox utilities.
    Move #root styles to index.css for better organization.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
-import './App.css'
 import Header from './components/header/Header'
 import Guessing from './components/guessing/Guessing'
 
 function App() {
   return (
-    <div className='app-container'>
+    <div className='flex flex-col items-center w-[550px] gap-20'>
       <Header />
       <Guessing />
     </div>
